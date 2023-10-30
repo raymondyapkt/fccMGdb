2be7b918c3bd2be2955546b95cce51cce25a2dae
raymondyapkt <134203379+raymondyapkt@users.noreply.github.com>
2023-10-30 14:57:06 +0800

    231029

--- myApp.js
+++ myApp.js
@@ -1,21 +1,52 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+/** 2) Create a 'Person' Model */
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+});
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+/** 3) Create and Save a Person */
+const Person = mongoose.model('Person', personSchema);
+const  createAndSavePerson = function(done) {
+  const janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+
+/** 4) Create many People with `Model.create()` */
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
+var createManyPeople = function(arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
+
+
+/** 5) Use `Model.find()` */
+var findPeopleByName = function(personName, done) {
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
+}; 
+
 
 const findOneByFood = (food, done) => {
   done(null /*, data*/);
